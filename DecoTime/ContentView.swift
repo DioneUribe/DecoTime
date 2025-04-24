12eabc6940e8847ce05c6ac75e4c59bfd898f200
Dione Uribe <ee1206@hotmail.com>
2025-04-24 13:24:22 -0600

    Initial Commit

---
+++ DecoTime/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DecoTime
+//
+//  Created by Dione Uribe on 24/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DecoTime/DecoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecoTimeApp.swift
+//  DecoTime
+//
+//  Created by Dione Uribe on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DecoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
